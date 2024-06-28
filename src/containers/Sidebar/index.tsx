6f04f38c5f2c83120c18005f37961b483c9e73f1
Gawevi <gbielwade@gmail.com>
2024-06-28 15:15:34 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+import { BotaoTema, Descricao, SidebarContainer } from "./styles"
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/gawevi.png" />
-    <Titulo fontSize={20}>Gawevi</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Gawevi</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        @gawevi
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Aspirante a Escritor e Estudante de Programação
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
